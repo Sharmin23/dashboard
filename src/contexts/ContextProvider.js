d75c88613bb06b4db9cc7fff648bcaae60610c52
Sharmin Sultana <sharmin9518@gmail.com>
2024-12-04 10:38:42 +0600

    first commit

---
+++ src/contexts/ContextProvider.js
@@ -0,0 +1,22 @@
+import React,{Children, createContet,createContext,useContext,useState} from 'react';
+
+const StateContext = createContext();
+
+const initialState = {
+    chat:false,
+    cart:false,
+    userProfile:false,
+    Notification: false,
+
+}
+
+export const ContextProvider = ({children}) => {
+    const [activeMenu,setActiveMenu] = useState(true);
+    return (
+        <StateContext.Provider value ={{activeMenu,setActiveMenu,}}>
+            {children}
+        </StateContext.Provider>
+    )
+}
+
+export const useStateConte xt = () => useContext(StateContext);
